5b3dfd4619f1727f1f23a7a99ae9bb5c4b437967
richard-dinh <richarddinh@gmail.com>
2021-02-20 13:01:06 -0800

    setup google search route

--- routes/googleRoutes.js
+++ routes/googleRoutes.js
@@ -1,7 +1,27 @@
 const router = require('express').Router()
 
+const {google} = require('googleapis')
+
+
 router.get('/google', (request, response) => {
-    response.json({message: `API: ${process.env.GOOGLE_API_KEY} CX: ${prcoess.env.CX_ID}`})
+    // response.json({message: `API: ${process.env.GOOGLE_API_KEY} CX: ${process.env.CX_ID}`})
+
+    let customsearch = google.customsearch('v1')
+
+    customsearch.cse.list({
+        auth: process.env.GOOGLE_API_KEY,
+        cx: process.env.CX_ID,
+        q: 'spongebob meme',
+        searchType: 'image',
+        // filetype filter (ommit if want all image types)
+        // fileType: ''
+    })
+    .then(({data}) => {
+        response.json(data)
+    })
+    .catch(err => {
+        response.status(400).send(err)
+    })
 })
 
 module.exports = router
