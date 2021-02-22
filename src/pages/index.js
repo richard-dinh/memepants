657b36bfc1d169663f2be2aede0b3539366cd790
richard-dinh <richarddinh@gmail.com>
2021-02-22 15:03:33 -0800

    create Header component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import { Link } from "gatsby"
 
 import API from '../utils/API/api'
 // Components
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import Header from '../components/Header/Header'
 
+// css
+import './index.css'
 
 const IndexPage = () => {
 
@@ -23,29 +23,7 @@ const IndexPage = () => {
 	
 
 	return(
-		<Layout>
-			<SEO title="Home" />
-			<h1>Hi people</h1>
-			<p>Welcome to your new Gatsby site.</p>
-			<p>Now go build something great.</p>
-			<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-				<Image />
-			</div>
-			<Link to="/page-2/">Go to page 2</Link> <br />
-			<Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-            {search ?
-                <ul>
-                {search.items.map(result => {
-                    return(
-                        <li>
-                            <img src = {result.link} alt = {result.title}></img>
-                            <p>{result.title}</p>
-                        </li>
-                    )
-                })}
-                </ul> 
-            : null}
-		</Layout>
+        <Header/>
 	)
 }
 
