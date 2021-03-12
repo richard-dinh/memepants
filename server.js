e7704a45081f10b5ef91e14e7f5a815d43df63ce
richard-dinh <richarddinh@gmail.com>
2021-03-12 14:30:48 -0800

    added port env

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.json())
 app.use(require('./routes'))
 
 
-app.listen(8001, () => {
+app.listen(process.env.PORT || 8001, () => {
     console.log('server listening to port 8001')
 })
