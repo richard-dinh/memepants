0b3a2568ea5d38e76fc19eedb065dda55ec6e665
richard-dinh <richarddinh@gmail.com>
2021-03-12 21:36:14 -0800

    update static.json

--- server.js
+++ server.js
@@ -10,6 +10,4 @@ app.use(express.json())
 app.use(require('./routes'))
 
 
-app.listen(process.env.PORT || 8001, () => {
-    console.log('server listening to port 8001')
-})
+app.listen(process.env.PORT || 8001)
