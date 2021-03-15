d3536ba9aea6d787a694c0eee900b798b8201e52
richard-dinh <richarddinh@gmail.com>
2021-03-15 12:59:07 -0700

    reder landing page and added publicPath

--- src/server/server.js
+++ src/server/server.js
@@ -1,11 +1,17 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 
 const app = express()
+const publicPath = path.join(__dirname, '../../', 'public')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
-
+app.use(express.static(publicPath))
+// render landing page
+app.get("", (req, res) => {
+  res.sendfile(path.join(publicPath, "index.html"))
+})
 
 app.use(require('./routes'))
 
