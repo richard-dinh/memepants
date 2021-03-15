53ba1a019cb59b55a65fa56e3baec1878aa56bcd
richard-dinh <richarddinh@gmail.com>
2021-03-15 13:38:44 -0700

    set up server for deployment

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 require('dotenv').config()
 const express = require('express')
 const { join } = require("path")
+
 const app = express()
+const publicPath = join(__dirname, 'public')
 
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+// server up files in public
+app.use(express.static(publicPath))
 
+// route to homepage
+app.get("/", (req, res) => {
+  res.sendfile(path.join(publicPath, "index.html"))
+})
 
 app.use(require('./routes'))
 
-app.get("/*", (request, response) => {
-  response.sendFile(join(__dirname, "public", "index.html"))
-})
 
 app.listen(process.env.PORT || 8001)
