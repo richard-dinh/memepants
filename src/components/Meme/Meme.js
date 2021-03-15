3851f09bcbaf71d277281c66ffadb950e42f0ce9
richard-dinh <richarddinh@gmail.com>
2021-03-15 14:11:23 -0700

    implement copy to clipboard

--- src/components/Meme/Meme.js
+++ src/components/Meme/Meme.js
@@ -5,9 +5,17 @@ import './Meme.css'
 
 const Meme = (props) => {
 
+    const handleOnClick = (link) => {
+        const tempDiv = document.createElement('textarea')
+        tempDiv.value = link
+        document.body.appendChild(tempDiv)
+        tempDiv.select()
+        document.execCommand('copy')
+        document.body.removeChild(tempDiv)
+    }
     return (
       <>
-        <div className="meme_card">
+        <div className="meme_card"  key = {props.link} onClick = {() => handleOnClick(props.link)}>
                 <img src= {props.link} alt="example"></img>
                 <p className = 'meme_text'>
                         {props.link}
